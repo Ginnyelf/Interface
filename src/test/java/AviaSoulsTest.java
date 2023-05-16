2d35ce859ead95bd4c8daf53bcec649608eb9799
Ginnyelf <ginnyelf0801@gmail.com>
2023-05-16 18:53:53 +0300

    add tests

--- src/test/java/AviaSoulsTest.java
+++ src/test/java/AviaSoulsTest.java
@@ -12,11 +12,13 @@ public class AviaSoulsTest {
     AviaSouls manager = new AviaSouls();
     private Ticket first = new Ticket("Пулково", "Кольцево", 2500, 12, 16);
     private Ticket second = new Ticket("Пулково", "Кольцево", 4000, 12, 17);
+    private Ticket third = new Ticket("Санкт-Петербург", "Москва", 4000, 12, 17);
 
     @BeforeEach
     public void setup() {
         manager.add(first);
         manager.add(second);
+        manager.add(third);
     }
 
     // Сортировка предложений по цене;
@@ -39,12 +41,30 @@ public class AviaSoulsTest {
 
     }
 
+    @Test
+    public void shouldSearchByMethodNotFound() {
+        Ticket[] expected = new Ticket[]{};
+        Ticket[] actual = manager.search("Пулково", "Москва");
+
+        Assertions.assertArrayEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldSearchByMethodFoundЩOnlyOne() {
+        Ticket[] expected = new Ticket[]{third};
+        Ticket[] actual = manager.search("Санкт-Петербург", "Москва");
+
+        Assertions.assertArrayEquals(actual, expected);
+
+    }
+
     // Сортировка предложений по цене,метод searchAndSortBy;
     @Test
     public void shouldSearchAndSortByByMethod() {
         Comparator<Ticket> comparator = new TicketTimeComparator();
         Ticket[] expected = new Ticket[]{first, second};
-        Ticket[] actual = manager.searchAndBySort("Пулково", "Кольцево",comparator);
+        Ticket[] actual = manager.searchAndBySort("Пулково", "Кольцево", comparator);
 
         Assertions.assertArrayEquals(actual, expected);
 
